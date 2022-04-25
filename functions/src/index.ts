41d5b8ea1275306f883ebd031fd648689ddd1e9c
Mayank Yadav <myyadavmayank1998@gmail.com>
2022-04-26 03:25:52 +0530

    function done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,29 +4,59 @@ import * as admin from "firebase-admin";
 const app = admin.initializeApp();
 // const app = !admin.apps.length ? admin.initializeApp() : admin.app();
 
-// const db = app.firestore();
+const db = app.firestore();
 const fcm = app.messaging();
 
 export const notificationManager = functions.firestore
   .document("chatroom/{chatRoomId}/chats/{chatID}")
   .onCreate(async (snap, context) => {
     const data = snap.data();
-    // const userData = await db
-    //   .collection("users")
-    //   .where("number", "==", data.recievedBy)
-    //   .get();
-    // const tokens = userData.docs.map((snap) => {
-    //   return snap.data().token;
-    // });
-    const payload = {
-      token:
-        "dJhkHqscQLaxlZwDvWRcQs:APA91bEUJi0vTGty_GaJzhOVx65XP1Hr82xyprSD87iBJRl_vT4lCPQwcYggz09IluRngq-NXJbxNfOzPde-pTAmJySLvDNcQ7pHMHKIbvr0KUiiWJX3cYq4FWcensyG9IYpx7N1zH1p",
+    const userData = await db
+      .collection("users")
+      .where("number", "==", data.recievedBy)
+      .get();
+    const tokens = userData.docs.map((snap) => {
+      return snap.data().token;
+    });
+    return fcm.sendMulticast({
+      tokens: tokens,
+
       notification: {
-        title: "New Order!",
-        body: `you got a message from ${data.sendByName} & ${data.message}`,
-        icon: "@mipmap/launcher_icon",
-        click_action: "FLUTTER_NOTIFICATION_CLICK",
+        title: "kiya konnect",
+
+        body: `${data.message} by ${data.sendByName}`,
+
+        imageUrl:
+          "https://firebasestorage.googleapis.com/v0/b/chatspro-364e3.appspot.com/o/appiconkk.png?alt=media&token=e461c545-78d6-4329-bc19-14f74212c2ec",
       },
-    };
-    return fcm.send(payload);
+    });
+  });
+
+export const groupNotificationManager = functions.firestore
+  .document("groups/{groupID}/chats/{chatID}")
+  .onCreate(async (snap, context) => {
+    const data = snap.data();
+    const group = context.params.groupID;
+    const usersFromGroup = await db.collection("groups").doc(group).get();
+    const users = usersFromGroup.data()!.members;
+    const filteredUsers = users.filter(
+      (user: { number: any }) => user.number != data.sendBy
+    );
+
+    const tokens = filteredUsers.map((snap: { token: any }) => {
+      return snap.token;
+    });
+
+    return fcm.sendMulticast({
+      tokens: tokens,
+
+      notification: {
+        title: "kiya konnect",
+
+        body: `${data.message} by ${data.sendByName}`,
+
+        imageUrl:
+          "https://firebasestorage.googleapis.com/v0/b/chatspro-364e3.appspot.com/o/appiconkk.png?alt=media&token=e461c545-78d6-4329-bc19-14f74212c2ec",
+      },
+    });
   });
